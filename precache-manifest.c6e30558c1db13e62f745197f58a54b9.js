0e7882327e6388725ba7a30e149de46942e2068f
Lovisa <elinlovisa.berglund@gmail.com>
2019-12-12 13:23:52 -0800

    Updates

---
+++ precache-manifest.c6e30558c1db13e62f745197f58a54b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6eb126a775420f6ad0d06d5f1a09f0",
+    "url": "/499_Final/index.html"
+  },
+  {
+    "revision": "61a156a678ad95b7719c",
+    "url": "/499_Final/static/css/main.b533a980.chunk.css"
+  },
+  {
+    "revision": "50ed106780f6ff261ebf",
+    "url": "/499_Final/static/js/2.2bc869ac.chunk.js"
+  },
+  {
+    "revision": "bc4ea76a8ec636f1aa592b58bb5d318f",
+    "url": "/499_Final/static/js/2.2bc869ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61a156a678ad95b7719c",
+    "url": "/499_Final/static/js/main.1282e345.chunk.js"
+  },
+  {
+    "revision": "8974afc427edc3c07575",
+    "url": "/499_Final/static/js/runtime-main.2bf8be26.js"
+  }
+]);
